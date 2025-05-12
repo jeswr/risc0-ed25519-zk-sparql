45a9892c39da6db64184a26b77f787cfc052afc7
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-05-12 02:06:58 +0100

    chore: fix signature package overrides

--- methods/guest-ed25519-verify/src/main.rs
+++ methods/guest-ed25519-verify/src/main.rs
@@ -4,6 +4,7 @@ use core::verify::{VerifyInput, ed25519_verify};
 fn main() {
     let input: VerifyInput = env::read();
     let canonical_document = input.canonical_document.clone();
+    let public_key = input.public_key.clone();
     ed25519_verify(input).expect("Signature verification failed");
-    env::commit(&canonical_document);
+    env::commit((&canonical_document, &public_key));
 }
