ea8cd2ed959961de8318d1e841845e6ab621af8f
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-05-12 01:59:50 +0100

    chore: disable dev mode

--- run_release.sh
+++ run_release.sh
@@ -8,9 +8,9 @@ do
     do
         echo "
         PROOF GENERATION (DATA: $dir) (QUERY: $query)"
-        time (RISC0_DEV_MODE=1 ./target/release/host -p $dir --mode prove --output-file ./sparql_result.json)
+        time (./target/release/host -p $dir --mode prove --output-file ./sparql_result.json)
         echo "
         VERIFICATION (DATA: $dir) (QUERY: $query)"
-        time (RISC0_DEV_MODE=1 ./target/release/host --mode verify --output-file ./sparql_result.json)
+        time (./target/release/host --mode verify --output-file ./sparql_result.json)
     done
 done
