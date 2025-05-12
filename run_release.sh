2e5ccbddfa365724e657ec4921301a0abee0909b
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-05-12 01:58:53 +0100

    chore: fixup benchmark script

--- run_release.sh
+++ run_release.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Iterate through the directories ./data/generated/ed25519-preprocessed/ and ./minimal
-# Iterate through the queries in ./data/queries/ in addition to ./query.sparql
+cargo build --release
 
 for dir in ./minimal/ ./data/generated/ed25519-preprocessed/
 do
     for query in ./data/queries/can-drive.rq ./data/queries/employment-status.rq ./query.sparql
     do
-        /usr/bin/time bash -c "echo 'PROOF GENERATION (DATA: $dir) (QUERY: $query)' && ./target/release/host -p $dir --mode prove --output-file ./sparql_result.json"
-        /usr/bin/time bash -c "echo 'VERIFICATION (DATA: $dir) (QUERY: $query)' && ./target/release/host --mode verify --output-file ./sparql_result.json"
+        echo "
+        PROOF GENERATION (DATA: $dir) (QUERY: $query)"
+        time (RISC0_DEV_MODE=1 ./target/release/host -p $dir --mode prove --output-file ./sparql_result.json)
+        echo "
+        VERIFICATION (DATA: $dir) (QUERY: $query)"
+        time (RISC0_DEV_MODE=1 ./target/release/host --mode verify --output-file ./sparql_result.json)
     done
 done
