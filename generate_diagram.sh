bb3666cd80de9ff4896c4789172f41b1e6938ae5
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-05-14 04:04:46 +0100

    feat: add distinct prover verifier diagrams

--- generate_diagram.sh
+++ generate_diagram.sh
@@ -17,6 +17,12 @@ mmdc -i architecture.mmd -o architecture.pdf -b white
 echo "Generating PNG from Mermaid diagram..."
 mmdc -i architecture.mmd -o architecture.png -b white -w 3000
 
+echo "Generating PNG from Mermaid diagram..."
+mmdc -i verifier.mmd -o verifier.png -b white -w 3000
+
+echo "Generating PNG from Mermaid diagram..."
+mmdc -i prover.mmd -o prover.png -b white -w 3000
+
 # Check if generation was successful
 if [ $? -eq 0 ]; then
     echo "Diagrams generated successfully at architecture.svg and architecture.pdf"
